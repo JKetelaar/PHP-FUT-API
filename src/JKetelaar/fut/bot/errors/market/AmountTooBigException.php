f2b46e0286a77729533b69755b3951b851096d84
JKetelaar <jeroen@ketelaar.me>
2016-10-14 01:42:09 +0200

    [FEATURE] Added AmountTooBigException for limits

---
+++ src/JKetelaar/fut/bot/errors/market/AmountTooBigException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author JKetelaar
+ */
+
+namespace JKetelaar\fut\bot\errors\market;
+
+class AmountTooBigException extends \Exception {
+
+    /**
+     * AmountTooBigException constructor.
+     *
+     * @param int $requested
+     * @param int $max
+     */
+    public function __construct($requested, $max) {
+        parent::__construct('You requested an amount of ' . $requested . ', though the limit is ' . $max);
+    }
+}
